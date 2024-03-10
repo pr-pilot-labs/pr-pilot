3277677012b6013dc0eb77016fa03039934b4428
Marco Lamina <mlamina09@gmail.com>
2024-03-09 18:17:02 -0800

    Fix k8s job

--- engine/job.py
+++ engine/job.py
@@ -1,3 +1,6 @@
+import logging
+import os
+
 import yaml
 from jinja2 import FileSystemLoader, Environment, select_autoescape
 from kubernetes import config
@@ -6,6 +9,8 @@ from kubernetes.client import BatchV1Api
 from engine.util import slugify
 
 
+logger = logging.getLogger(__name__)
+
 class KubernetesJob:
 
     def __init__(self, task):
@@ -13,29 +18,32 @@ class KubernetesJob:
 
     def spawn(self):
         # Load kubeconfig
-        config.load_kube_config()
+        try:
+            config.load_kube_config()
+        except config.config_exception.ConfigException:
+            config.load_incluster_config()
 
         # Load the Jinja2 template
-        file_loader = FileSystemLoader('.')
+        this_dir = os.path.dirname(os.path.abspath(__file__))
+        file_loader = FileSystemLoader(this_dir)
         env = Environment(loader=file_loader, autoescape=select_autoescape())
         template = env.get_template('job_template.yaml.j2')
-        labels = {
-            "task_id": str(self.task.id),
-            "github_project": self.task.github_project,
-            "github_user": self.task.github_user,
-            "branch": self.task.branch,
-
-        }
         # Render the template with variables
+        github_project_org, github_project_name = self.task.github_project.split("/")
+        job_name = slugify(self.task.title)
+        job_name = "job-" + job_name[:50]
         rendered_template = template.render(
-            job_name=slugify(self.task.title),
-            labels=labels,
+            job_name=job_name,
+            github_user=self.task.github_user,
+            github_project_org=github_project_org,
+            github_project_name=github_project_name,
+            branch=self.task.branch,
             task_id=str(self.task.id),
         )
 
         # Load the rendered template as a Kubernetes object
         job_object = yaml.safe_load(rendered_template)
-
+        logger.info(f"Job created: {job_name}")
         # Create the job
         batch_v1 = BatchV1Api()
         return batch_v1.create_namespaced_job(body=job_object, namespace="default")
