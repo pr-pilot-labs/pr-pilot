99345007ff4cfddea79b78c62a4fe1d64f1829ea
root <root@job-3a31e586-20d6-488b-bb0b-03793cd02d6d-pd7pz>
2024-03-14 03:36:47 +0000

    Refactor: Update TaskEngine class to use new structure

--- engine/task_engine.py
+++ engine/task_engine.py
@@ -1,23 +1,14 @@
 import logging
-import os
-import shutil
 from decimal import Decimal
-
-import git
-from django.conf import settings
 from django.db.models import Sum
-from git import Repo
-from github import Github, GithubException
 from github.PullRequest import PullRequest
-
 from accounts.models import UserBudget
 from engine.agents.pr_pilot_agent import create_pr_pilot_agent
-from engine.langchain.generate_pr_info import generate_pr_info, LabelsAndTitle
-from engine.langchain.generate_task_title import generate_task_title
 from engine.models import Task, TaskEvent, CostItem
 from engine.project import Project
-from engine.util import slugify
-from webhooks.jwt_tools import get_installation_access_token
+from engine.project_manager import ProjectManager
+from engine.github_client import GitHubClient
+from engine.task_utils import generate_task_title_wrapper
 
 logger = logging.getLogger(__name__)
 
@@ -27,84 +18,22 @@ class TaskEngine:
         self.task = task
         self.max_steps = max_steps
         self.executor = create_pr_pilot_agent()
-        self.github_token = get_installation_access_token(self.task.installation_id)
-        self.github = Github(self.github_token)
-        self.github_repo = self.github.get_repo(self.task.github_project)
-        self.project = Project(name=self.github_repo.full_name, main_branch=self.github_repo.default_branch)
-
-
-    def create_unique_branch_name(self, basis: str):
-        """Create branch name based on a given string. If branch exists,
-        add increasing numbers at the end"""
-        unique_branch_name = f"pr-pilot/{slugify(basis)}"[:50]
-        repo = Repo(settings.REPO_DIR)
-
-        counter = 1
-        original_branch_name = unique_branch_name
-        while unique_branch_name in repo.branches:
-            unique_branch_name = f"{original_branch_name}-{counter}"
-            counter += 1
-        return unique_branch_name
-
-    def setup_working_branch(self, branch_name_basis: str):
-        """
-        Create a new branch based on the given branch name basis.
-        :param branch_name_basis: String to use for generating the branch name
-        :return: Branch name
-        """
-
-        self.project.discard_all_changes()
-        self.project.fetch_remote()
-        self.project.checkout_latest_default_branch()
-        tool_branch = self.create_unique_branch_name(branch_name_basis)
-        logger.info(f"Creating new local branch '{tool_branch}'...")
-        self.project.create_new_branch(tool_branch)
-        return tool_branch
-
-
-    def finalize_working_branch(self, branch_name: str) -> bool:
-        """
-        Finalize the working branch by committing and pushing changes.
-        :param branch_name: Name of the working branch
-        :return: True if changes were pushed, False if no changes were made
-        """
-        if self.project.has_uncommitted_changes():
-            logger.warn(f"Found uncommitted changes on {branch_name!r} branch! Committing...")
-            self.project.commit_all_changes(message=f"Uncommitted changes")
-        if self.project.get_diff_to_main():
-            logger.info(f"Found changes on {branch_name!r} branch. Pushing and creating PR...")
-            self.project.push_branch(branch_name)
-            TaskEvent.add(actor="assistant", action="push_branch", target=branch_name)
-            self.project.checkout_latest_default_branch()
-            return True
-        else:
-            logger.info(f"No changes on {branch_name} branch. Deleting...")
-            self.project.checkout_latest_default_branch()
-            self.project.delete_branch(branch_name)
-            return False
-
-    def generate_task_title(self):
-        if self.task.pr_number:
-            pr = self.github_repo.get_pull(self.task.pr_number)
-            self.task.title = generate_task_title(pr.body, self.task.user_request)
-        else:
-            issue = self.github_repo.get_issue(self.task.issue_number)
-            self.task.title = generate_task_title(issue.body, self.task.user_request)
-        self.task.save()
-
+        self.project_manager = ProjectManager(Project(name=task.github_project, main_branch=task.default_branch))
+        self.github_client = GitHubClient(task.installation_id, task.github_project)
+        self.project = Project(name=task.github_project, main_branch=task.default_branch)
 
     def run(self) -> str:
         self.task.status = "running"
         self.task.save()
-        self.generate_task_title()
-        self.clone_github_repo()
+        generate_task_title_wrapper(self.task)
+        self.project_manager.clone_github_repo()
         # If task is a PR, checkout the PR branch
         if self.task.pr_number:
             TaskEvent.add(actor="assistant", action="checkout_pr_branch", target=self.task.head, message="Checking out PR branch")
             self.project.checkout_branch(self.task.head)
             working_branch = self.task.head
         else:
-            working_branch = self.setup_working_branch(self.task.title)
+            working_branch = self.project_manager.setup_working_branch(self.task.title)
         try:
             # Make sure we never work directly on the main branch
             if self.project.active_branch == self.project.main_branch:
@@ -118,14 +47,10 @@ class TaskEngine:
                 if self.project.get_diff_to_main():
                     logger.info(f"Found changes on {working_branch!r} branch. Pushing ...")
                     self.project.push_branch(working_branch)
-            elif working_branch and self.finalize_working_branch(working_branch):
+            elif working_branch and self.project_manager.finalize_working_branch(working_branch):
                 # We are working on a new branch and have changes to push
                 logger.info(f"Creating pull request for branch {working_branch}")
-                pr_info = generate_pr_info(final_response)
-                if not pr_info:
-                    pr_info = LabelsAndTitle(title=self.task.title, labels=["pr-pilot"])
-                pr: PullRequest = Project.from_github().create_pull_request(title=pr_info.title, body=final_response,
-                                                               head=working_branch, labels=pr_info.labels)
+                pr: PullRequest = self.project.create_pull_request(title=self.task.title, body=final_response, head=working_branch)
                 final_response += f"\n\n**PR**: [{pr.title}]({pr.html_url})\n\nIf you require further changes, continue our conversation over there!"
             final_response += f"\n\n📋[Task Log](https://app.pr-pilot.ai/dashboard/tasks/{str(self.task.id)}/)"
         except Exception as e:
@@ -147,14 +72,3 @@ class TaskEngine:
                 logger.warning(f"No cost items found for task {self.task.title}")
         self.task.response_comment.edit(final_response)
         return final_response
-
-    def clone_github_repo(self):
-        TaskEvent.add(actor="assistant", action="clone_repo", target=self.task.github_project, message="Cloning repository")
-        logger.info(f"Cloning repo {self.task.github_project} to {settings.REPO_DIR}")
-        if os.path.exists(settings.REPO_DIR):
-            logger.info(f"Deleting existing directory contents.")
-            shutil.rmtree(settings.REPO_DIR)
-        git_repo_url = f'https://x-access-token:{self.github_token}@github.com/{self.task.github_project}.git'
-        git.Repo.clone_from(git_repo_url, settings.REPO_DIR)
-        logger.info(f"Cloned repo {self.task.github_project} to {settings.REPO_DIR}")
-
